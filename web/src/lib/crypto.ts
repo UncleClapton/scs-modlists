864caad41d5984bec6fe0efc2bb0df2c286335c0
Cameron Welter <camjwelt@gmail.com>
2026-06-11 20:36:28 -0400

    refactor: remove dead code

--- web/src/lib/crypto.ts
+++
@@ -1,8 +0,0 @@
-export async function getBase64Hash(input: string): Promise<string> {
-  const hashBuffer = await window.crypto.subtle.digest(
-    "SHA-256",
-    new TextEncoder().encode(input),
-  );
-  const base64 = new Uint8Array(hashBuffer).toBase64();
-  return base64.replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
-}
